569c492c276be08fe89eb9c154057f2ed6738235
josanjd <josanjd96@hotmail.com>
2020-08-15 13:21:21 +0200

    Variable de entorno MONGO_URI

--- server/config/congif.js
+++ server/config/congif.js
@@ -22,7 +22,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/coffe'
 } else {
-    urlDB = 'mongodb+srv://strider:47fnnDAug38Je5hV@cluster0.apzof.mongodb.net/test'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
